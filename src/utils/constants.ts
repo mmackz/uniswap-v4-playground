2ffb72a441889b3579b0433a597f4117d92776c3
mmackz <matthewmackenz@gmail.com>
2025-05-21 23:11:06 -0700

    add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const flETH = '0x000000000d564d5be76f7f0d28fe52605afc7cf8' as const
+export const flETH_Hooks = '0x9E433F32bb5481a9CA7DFF5b3af74A7ed041a888' as const
+export const positionManager = '0xF785bb58059FAB6fb19bDdA2CB9078d9E546Efdc' as const
+export const UNIVERSAL_ROUTER = '0x6ff5693b99212da76ad316178a184ab56d299b43' as const
+export const QUESTION = '0x3025e7854482bcfb770b0b204a6a8ad11c5152a9' as const
+export const COMMAND_V4_SWAP = '0x10' as const
+export const COMMAND_V4_SWAP_AND_SWEEP = '0x1004' as const
+
+// SWAP_EXACT_IN | SETTLE_ALL | TAKE_ALL
+export const ACTIONS = '0x070c0f' as const 
