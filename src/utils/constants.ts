a3a0a6f789be35efe9d556186b71f696b334f29a
mmackz <matthewmackenz@gmail.com>
2025-05-22 13:57:41 -0700

    comment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const flETH_Hooks = '0x9E433F32bb5481a9CA7DFF5b3af74A7ed041a888' as const
 export const positionManager = '0xF785bb58059FAB6fb19bDdA2CB9078d9E546Efdc' as const
 export const UNIVERSAL_ROUTER = '0x6ff5693b99212da76ad316178a184ab56d299b43' as const
 export const QUOTER_ADDRESS = '0x0d5e0f971ed27fbff6c2837bf31316121532048d' as const
-export const QUESTION = '0x3025e7854482bcfb770b0b204a6a8ad11c5152a9' as const
+export const QUESTION = '0x3025e7854482bcfb770b0b204a6a8ad11c5152a9' as const // ! use Together token in production
 export const COMMAND_V4_SWAP = '0x10' as const
 export const COMMAND_V4_SWAP_AND_SWEEP = '0x1004' as const
 
