5c36dfbae90cbfa473497814d90d5173cc756e85
mmackz <matthewmackenz@gmail.com>
2025-05-21 23:10:58 -0700

    add abi and abiparams

--- src/utils/abi.ts
+++ src/utils/abi.ts
@@ -2,186 +2,6 @@ export const QUOTER_V4_ABI = [
   {
     inputs: [
       {
-        internalType: 'contract IPoolManager',
-        name: '_poolManager',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-  },
-  {
-    inputs: [{ internalType: 'PoolId', name: 'poolId', type: 'bytes32' }],
-    name: 'NotEnoughLiquidity',
-    type: 'error',
-  },
-  { inputs: [], name: 'NotPoolManager', type: 'error' },
-  { inputs: [], name: 'NotSelf', type: 'error' },
-  {
-    inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
-    name: 'QuoteSwap',
-    type: 'error',
-  },
-  { inputs: [], name: 'UnexpectedCallSuccess', type: 'error' },
-  {
-    inputs: [{ internalType: 'bytes', name: 'revertData', type: 'bytes' }],
-    name: 'UnexpectedRevertBytes',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        components: [
-          { internalType: 'Currency', name: 'exactCurrency', type: 'address' },
-          {
-            components: [
-              {
-                internalType: 'Currency',
-                name: 'intermediateCurrency',
-                type: 'address',
-              },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-              { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-            ],
-            internalType: 'struct PathKey[]',
-            name: 'path',
-            type: 'tuple[]',
-          },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactParams',
-        name: 'params',
-        type: 'tuple',
-      },
-    ],
-    name: '_quoteExactInput',
-    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        components: [
-          {
-            components: [
-              { internalType: 'Currency', name: 'currency0', type: 'address' },
-              { internalType: 'Currency', name: 'currency1', type: 'address' },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-            ],
-            internalType: 'struct PoolKey',
-            name: 'poolKey',
-            type: 'tuple',
-          },
-          { internalType: 'bool', name: 'zeroForOne', type: 'bool' },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-          { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactSingleParams',
-        name: 'params',
-        type: 'tuple',
-      },
-    ],
-    name: '_quoteExactInputSingle',
-    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        components: [
-          { internalType: 'Currency', name: 'exactCurrency', type: 'address' },
-          {
-            components: [
-              {
-                internalType: 'Currency',
-                name: 'intermediateCurrency',
-                type: 'address',
-              },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-              { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-            ],
-            internalType: 'struct PathKey[]',
-            name: 'path',
-            type: 'tuple[]',
-          },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactParams',
-        name: 'params',
-        type: 'tuple',
-      },
-    ],
-    name: '_quoteExactOutput',
-    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        components: [
-          {
-            components: [
-              { internalType: 'Currency', name: 'currency0', type: 'address' },
-              { internalType: 'Currency', name: 'currency1', type: 'address' },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-            ],
-            internalType: 'struct PoolKey',
-            name: 'poolKey',
-            type: 'tuple',
-          },
-          { internalType: 'bool', name: 'zeroForOne', type: 'bool' },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-          { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactSingleParams',
-        name: 'params',
-        type: 'tuple',
-      },
-    ],
-    name: '_quoteExactOutputSingle',
-    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'poolManager',
-    outputs: [
-      { internalType: 'contract IPoolManager', name: '', type: 'address' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
         components: [
           { internalType: 'Currency', name: 'exactCurrency', type: 'address' },
           {
@@ -219,332 +39,54 @@ export const QUOTER_V4_ABI = [
     stateMutability: 'nonpayable',
     type: 'function',
   },
+] as const
+
+export const V4_EXECUTE_ABI = [
   {
+    name: 'execute',
+    type: 'function',
+    stateMutability: 'payable',
     inputs: [
-      {
-        components: [
-          {
-            components: [
-              { internalType: 'Currency', name: 'currency0', type: 'address' },
-              { internalType: 'Currency', name: 'currency1', type: 'address' },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-            ],
-            internalType: 'struct PoolKey',
-            name: 'poolKey',
-            type: 'tuple',
-          },
-          { internalType: 'bool', name: 'zeroForOne', type: 'bool' },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-          { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactSingleParams',
-        name: 'params',
-        type: 'tuple',
-      },
+      { internalType: 'bytes', name: 'commands', type: 'bytes' },
+      { internalType: 'bytes[]', name: 'inputs', type: 'bytes[]' },
     ],
-    name: 'quoteExactInputSingle',
-    outputs: [
-      { internalType: 'uint256', name: 'amountOut', type: 'uint256' },
-      { internalType: 'uint256', name: 'gasEstimate', type: 'uint256' },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        components: [
-          { internalType: 'Currency', name: 'exactCurrency', type: 'address' },
-          {
-            components: [
-              {
-                internalType: 'Currency',
-                name: 'intermediateCurrency',
-                type: 'address',
-              },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-              { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-            ],
-            internalType: 'struct PathKey[]',
-            name: 'path',
-            type: 'tuple[]',
-          },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactParams',
-        name: 'params',
-        type: 'tuple',
-      },
-    ],
-    name: 'quoteExactOutput',
-    outputs: [
-      { internalType: 'uint256', name: 'amountIn', type: 'uint256' },
-      { internalType: 'uint256', name: 'gasEstimate', type: 'uint256' },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        components: [
-          {
-            components: [
-              { internalType: 'Currency', name: 'currency0', type: 'address' },
-              { internalType: 'Currency', name: 'currency1', type: 'address' },
-              { internalType: 'uint24', name: 'fee', type: 'uint24' },
-              { internalType: 'int24', name: 'tickSpacing', type: 'int24' },
-              {
-                internalType: 'contract IHooks',
-                name: 'hooks',
-                type: 'address',
-              },
-            ],
-            internalType: 'struct PoolKey',
-            name: 'poolKey',
-            type: 'tuple',
-          },
-          { internalType: 'bool', name: 'zeroForOne', type: 'bool' },
-          { internalType: 'uint128', name: 'exactAmount', type: 'uint128' },
-          { internalType: 'bytes', name: 'hookData', type: 'bytes' },
-        ],
-        internalType: 'struct IV4Quoter.QuoteExactSingleParams',
-        name: 'params',
-        type: 'tuple',
-      },
-    ],
-    name: 'quoteExactOutputSingle',
-    outputs: [
-      { internalType: 'uint256', name: 'amountIn', type: 'uint256' },
-      { internalType: 'uint256', name: 'gasEstimate', type: 'uint256' },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'bytes', name: 'data', type: 'bytes' }],
-    name: 'unlockCallback',
-    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    outputs: [],
   },
 ] as const
 
-export const UNIVERSAL_ROUTER_ABI = [
+// ABI parameter definitions for EXACT_INPUT
+export const EXACT_INPUT_PARAMS = [
+  { type: 'address', name: 'currencyIn' },
   {
-    inputs: [
-      {
-        components: [
-          { internalType: 'address', name: 'permit2', type: 'address' },
-          { internalType: 'address', name: 'weth9', type: 'address' },
-          { internalType: 'address', name: 'v2Factory', type: 'address' },
-          { internalType: 'address', name: 'v3Factory', type: 'address' },
-          {
-            internalType: 'bytes32',
-            name: 'pairInitCodeHash',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'poolInitCodeHash',
-            type: 'bytes32',
-          },
-          { internalType: 'address', name: 'v4PoolManager', type: 'address' },
-          {
-            internalType: 'address',
-            name: 'v3NFTPositionManager',
-            type: 'address',
-          },
-          {
-            internalType: 'address',
-            name: 'v4PositionManager',
-            type: 'address',
-          },
-        ],
-        internalType: 'struct RouterParameters',
-        name: 'params',
-        type: 'tuple',
-      },
+    type: 'tuple[]',
+    name: 'path',
+    components: [
+      { type: 'address', name: 'intermediateCurrency' },
+      { type: 'uint24', name: 'fee' },
+      { type: 'int24', name: 'tickSpacing' },
+      { type: 'address', name: 'hooks' },
+      { type: 'bytes', name: 'hookData' },
     ],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
   },
-  { inputs: [], name: 'BalanceTooLow', type: 'error' },
-  { inputs: [], name: 'ContractLocked', type: 'error' },
-  {
-    inputs: [{ internalType: 'Currency', name: 'currency', type: 'address' }],
-    name: 'DeltaNotNegative',
-    type: 'error',
-  },
-  {
-    inputs: [{ internalType: 'Currency', name: 'currency', type: 'address' }],
-    name: 'DeltaNotPositive',
-    type: 'error',
-  },
-  { inputs: [], name: 'ETHNotAccepted', type: 'error' },
-  {
-    inputs: [
-      { internalType: 'uint256', name: 'commandIndex', type: 'uint256' },
-      { internalType: 'bytes', name: 'message', type: 'bytes' },
-    ],
-    name: 'ExecutionFailed',
-    type: 'error',
-  },
-  { inputs: [], name: 'FromAddressIsNotOwner', type: 'error' },
-  { inputs: [], name: 'InputLengthMismatch', type: 'error' },
-  { inputs: [], name: 'InsufficientBalance', type: 'error' },
-  { inputs: [], name: 'InsufficientETH', type: 'error' },
-  { inputs: [], name: 'InsufficientToken', type: 'error' },
-  {
-    inputs: [{ internalType: 'bytes4', name: 'action', type: 'bytes4' }],
-    name: 'InvalidAction',
-    type: 'error',
-  },
-  { inputs: [], name: 'InvalidBips', type: 'error' },
-  {
-    inputs: [{ internalType: 'uint256', name: 'commandType', type: 'uint256' }],
-    name: 'InvalidCommandType',
-    type: 'error',
-  },
-  { inputs: [], name: 'InvalidEthSender', type: 'error' },
-  { inputs: [], name: 'InvalidPath', type: 'error' },
-  { inputs: [], name: 'InvalidReserves', type: 'error' },
-  { inputs: [], name: 'LengthMismatch', type: 'error' },
-  {
-    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
-    name: 'NotAuthorizedForToken',
-    type: 'error',
-  },
-  { inputs: [], name: 'NotPoolManager', type: 'error' },
-  { inputs: [], name: 'OnlyMintAllowed', type: 'error' },
-  { inputs: [], name: 'SliceOutOfBounds', type: 'error' },
-  { inputs: [], name: 'TransactionDeadlinePassed', type: 'error' },
-  { inputs: [], name: 'UnsafeCast', type: 'error' },
-  {
-    inputs: [{ internalType: 'uint256', name: 'action', type: 'uint256' }],
-    name: 'UnsupportedAction',
-    type: 'error',
-  },
-  { inputs: [], name: 'V2InvalidPath', type: 'error' },
-  { inputs: [], name: 'V2TooLittleReceived', type: 'error' },
-  { inputs: [], name: 'V2TooMuchRequested', type: 'error' },
-  { inputs: [], name: 'V3InvalidAmountOut', type: 'error' },
-  { inputs: [], name: 'V3InvalidCaller', type: 'error' },
-  { inputs: [], name: 'V3InvalidSwap', type: 'error' },
-  { inputs: [], name: 'V3TooLittleReceived', type: 'error' },
-  { inputs: [], name: 'V3TooMuchRequested', type: 'error' },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'minAmountOutReceived',
-        type: 'uint256',
-      },
-      { internalType: 'uint256', name: 'amountReceived', type: 'uint256' },
-    ],
-    name: 'V4TooLittleReceived',
-    type: 'error',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'maxAmountInRequested',
-        type: 'uint256',
-      },
-      { internalType: 'uint256', name: 'amountRequested', type: 'uint256' },
-    ],
-    name: 'V4TooMuchRequested',
-    type: 'error',
-  },
-  {
-    inputs: [],
-    name: 'V3_POSITION_MANAGER',
-    outputs: [
-      {
-        internalType: 'contract INonfungiblePositionManager',
-        name: '',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'V4_POSITION_MANAGER',
-    outputs: [
-      { internalType: 'contract IPositionManager', name: '', type: 'address' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'bytes', name: 'commands', type: 'bytes' },
-      { internalType: 'bytes[]', name: 'inputs', type: 'bytes[]' },
-    ],
-    name: 'execute',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'bytes', name: 'commands', type: 'bytes' },
-      { internalType: 'bytes[]', name: 'inputs', type: 'bytes[]' },
-      { internalType: 'uint256', name: 'deadline', type: 'uint256' },
-    ],
-    name: 'execute',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'msgSender',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'poolManager',
-    outputs: [
-      { internalType: 'contract IPoolManager', name: '', type: 'address' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'int256', name: 'amount0Delta', type: 'int256' },
-      { internalType: 'int256', name: 'amount1Delta', type: 'int256' },
-      { internalType: 'bytes', name: 'data', type: 'bytes' },
-    ],
-    name: 'uniswapV3SwapCallback',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'bytes', name: 'data', type: 'bytes' }],
-    name: 'unlockCallback',
-    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  { stateMutability: 'payable', type: 'receive' },
+  { type: 'uint128', name: 'amountIn' },
+  { type: 'uint128', name: 'amountOutMinimum' },
+] as const
+
+// ABI parameter definitions for SETTLE_ALL
+export const SETTLE_ALL_PARAMS = [
+  { name: 'currency', type: 'address' },
+  { name: 'amount',   type: 'uint256' },
+] as const
+
+// ABI parameter definitions for TAKE_ALL
+export const TAKE_ALL_PARAMS = [
+  { name: 'currencyOut',       type: 'address' },
+  { name: 'amountOutMinimum', type: 'uint256' },
+] as const
+
+// ABI parameter definitions for SWEEP
+export const SWEEP_PARAMS = [
+  { name: 'token',     type: 'address' },
+  { name: 'recipient', type: 'address' },
+  { name: 'minAmount', type: 'uint256' },
 ] as const
